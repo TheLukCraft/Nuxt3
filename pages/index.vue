5076053b78b96b601d488a6fdc1d474f8fd03da4
TheLukCraft <luk1997r@o2.pl>
2024-05-27 03:20:50 +0200

    Adding Pages

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h2>Home</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet culpa ipsum alias autem minima ea quo placeat
+            natus quidem? Reprehenderit illo, neque non quidem fuga modi quod fugiat laboriosam eius?</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi atque possimus molestias recusandae magni quia
+            odit! Molestiae numquam deserunt quisquam, animi sunt ipsum, velit, magni reiciendis reprehenderit
+            perferendis beatae atque!</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+}
+
+p {
+    margin: 20px 0;
+}
+</style>
