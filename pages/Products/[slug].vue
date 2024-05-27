28a4804f500d5dfe978c379d224fe501e0f3658f
TheLukCraft <luk1997r@o2.pl>
2024-05-27 03:38:53 +0200

    Route-Parameters update

--- pages/Products/[slug].vue
+++ pages/Products/[slug].vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <p>Product details for {{ id }}</p>
+        <p>Product details for {{ slug }}</p>
         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex explicabo a impedit quasi commodi ad porro,
             ipsam est, inventore iusto harum repellendus. Natus amet repellat fugit aliquid quae debitis odit!</p>
     </div>
