2d784a9e3bb43ee15b35baf1c7aba8041d3b4b81
TheLukCraft <luk1997r@o2.pl>
2024-05-27 03:37:59 +0200

    Route-Parameters update

---
+++ pages/Products/[slug].vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <p>Product details for {{ id }}</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex explicabo a impedit quasi commodi ad porro,
+            ipsam est, inventore iusto harum repellendus. Natus amet repellat fugit aliquid quae debitis odit!</p>
+    </div>
+</template>
+
+<script setup>
+const { slug } = useRoute().params
+</script>
+
+<style scoped></style>
